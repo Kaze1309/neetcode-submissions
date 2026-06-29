825ad5179f0a391de4f0bcdb62f36025f77d4640
Sameer Pagar <127657064+Kaze1309@users.noreply.github.com>
2026-06-29 22:18:23 +0530

    Add: two-integer-sum - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum/submission-1.py
@@ -0,0 +1,8 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        h = {}
+        for i in range(len(nums)):
+            if target - nums[i] not in h:
+                h[nums[i]] = i    
+            else:
+                return [h[target - nums[i]], i]
